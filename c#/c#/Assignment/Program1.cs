dd99a8bb24e8dacb155e423325cf7a6d8cc2dcbf
Nazeem Rofina <nazeemrofina@gmail.com>
2025-03-28 14:48:05 +0530

    'Assignment1revised'

--- c#/c#/Assignment/Program1.cs
+++ c#/c#/Assignment/Program1.cs
@@ -4,9 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Program2 prg = new Program2();
-            prg.Checks();
-            Program3.Pro3();
+           
             Console.WriteLine("Enter a number a and b:");
             int a = Convert.ToInt32(Console.ReadLine());
             int b = Convert.ToInt32(Console.ReadLine());
@@ -18,7 +16,9 @@
             {
                 Console.WriteLine($"{a} NO THEY ARE DIFFERENT NUMBERS");
             }
-
+            Program2 prg = new Program2();
+            prg.Checks();
+            Program3.Pro3();
             Program4.Pro4();
             Program5.Pro5();
         }
